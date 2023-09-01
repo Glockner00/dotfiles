824e96d912c14afdce02c272889829ec99370dd7
Axel Glöckner <axel.glockner00@gmail.com>
2023-09-01 21:16:22 +0200

    zathura/snippets

--- nvim/lua/core/plugin_config/vimtex.lua
+++ nvim/lua/core/plugin_config/vimtex.lua
@@ -1,7 +1,7 @@
-vim.g.tex_flavor = 'latex' -- Set LaTeX as the default flavor
+vim.g.tex_flavor = 'latex'               -- Set LaTeX as the default flavor
 vim.g.vimtex_compiler_method = 'latexmk' -- Use latexmk for compilation
 vim.g.vimtex_auto_save = 1
 vim.g.vimtex_quickfix_mode = 0
 
-vim.keymap.set("n", "<leader>ll", ":VimtexCompile")
-vim.keymap.set("n", "<leader>ll", ":VimtexView")
+vim.keymap.set("n", "<leader>vc", ":VimtexCompile")
+vim.keymap.set("n", "<leader>vv", ":VimtexView")
--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -19,7 +19,7 @@ local plugins = {
         as = 'rose-pine',
         config = function()
             vim.cmd('colorscheme rose-pine')
-         end
+        end
     },
 
     {
@@ -27,21 +27,21 @@ local plugins = {
         branch = 'v2.x',
         dependencies = {
             -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {'williamboman/mason.nvim'},           -- Optional
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
             -- Autocompletion
-            {'hrsh7th/nvim-cmp'},     -- Required
-            {'hrsh7th/cmp-nvim-lsp'}, -- Required
-            {'L3MON4D3/LuaSnip'},     -- Required
+            { 'hrsh7th/nvim-cmp' },     -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' },     -- Required
         }
     },
 
     {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.0',
-        dependencies = { {'nvim-lua/plenary.nvim'} }
+        dependencies = { { 'nvim-lua/plenary.nvim' } }
     },
 
     {
@@ -77,9 +77,13 @@ local plugins = {
     },
 
     'godlygeek/tabular',
-    'lervag/vimtex',   -- PDF rendering
+    'lervag/vimtex', -- PDF rendering
+
+    -- UltiSnips for LaTeX snippets
+    'SirVer/ultisnips',
+
+    'honza/vim-snippets',
 }
 
 local opts = {}
 require("lazy").setup(plugins, opts)
-
